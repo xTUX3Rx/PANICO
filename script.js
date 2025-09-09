808dee43855874103555f68183c7caa309e90b73
xTUX3Rx <joseabantotrigoso@gmail.com>
2025-09-08 19:48:15 -0500

    Revert "Modificaciones luego de la Restauración"

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function setPoliceNumber() {
   if (locationConfig === "peru") {
     policeNumber = '105';
   }
-  document.getElementById('policeNumber').textContent = `Llamar a Policia`;
+  document.getElementById('policeNumber').textContent = `Llamar al ${policeNumber}`;
 }
 
 setPoliceNumber();
@@ -264,12 +264,3 @@ document.querySelector('.dialog-action').addEventListener('click', () => {
   toggleNotification();
   executeAlarm();
 });
-
-
-//boton salir
- function logout() {
-     localStorage.clear();
-      window.location.href = "../../..index.html";
-    }
-
-    $('#logoutBtn').addEventListener('click', logout);
