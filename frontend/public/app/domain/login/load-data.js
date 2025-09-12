e34355b423ac8f3af859020e34bfd77c9ff9069f
xTUX3Rx <joseabantotrigoso@gmail.com>
2025-09-12 16:40:31 -0500

    LOCAL STORAGE

--- frontend/public/app/domain/login/load-data.js
+++ frontend/public/app/domain/login/load-data.js
@@ -1,8 +1,9 @@
-// (function login() {
-//   fetch('../../backend/data/usuarios.json')
-//     .then(response => response.json())
-//     .then(data => {
-//       localStorage.setItem('usuario', JSON.stringify(data));
-//     })
-//     .catch(error => console.error('Error al cargar el JSON:', error));
-// })();
+//LOCAL STORAGE
+(function login(){
+   fetch('../../backend/data/usuarios.json')
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem('usuario', JSON.stringify(data));
+    })
+    .catch(error => console.error('Error al cargar el JSON:', error));
+})();
