f2e121386097aa53dd3536f817cc0740fdeef3a7
xTUX3Rx <joseabantotrigoso@gmail.com>
2025-09-12 14:33:28 -0500

    comentado load-data.js

--- frontend/public/app/domain/login/load-data.js
+++ frontend/public/app/domain/login/load-data.js
@@ -1,8 +1,8 @@
-(function login() {
-  fetch('../../backend/data/usuarios.json')
-    .then(response => response.json())
-    .then(data => {
-      localStorage.setItem('usuario', JSON.stringify(data));
-    })
-    .catch(error => console.error('Error al cargar el JSON:', error));
-})();
+// (function login() {
+//   fetch('../../backend/data/usuarios.json')
+//     .then(response => response.json())
+//     .then(data => {
+//       localStorage.setItem('usuario', JSON.stringify(data));
+//     })
+//     .catch(error => console.error('Error al cargar el JSON:', error));
+// })();
