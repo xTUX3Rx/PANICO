4db746f542b47a44f34d3c5bfd1ee8542ff828e7
xTUX3Rx <joseabantotrigoso@gmail.com>
2025-09-14 14:16:26 -0500

    username

--- frontend/public/app/domain/login/load-data.js
+++ frontend/public/app/domain/login/load-data.js
@@ -2,7 +2,7 @@
   fetch('/.netlify/functions/usuarios') // Debes crear esta función si quieres exponer los usuarios
     .then(response => response.json())
     .then(data => {
-      localStorage.setItem('usuario', JSON.stringify(data));
+      localStorage.setItem('username', JSON.stringify(data));
     })
     .catch(error => console.error('Error al cargar el JSON:', error));
 })();
