09ac6797c6502727d367c9c0c7159b6e3115976e
Kevin Rodriguez <kevindeywin@gmail.com>
2025-09-15 02:16:38 -0500

    feat: upate login logic

--- backend/functions/usuarios.js
+++ backend/functions/usuarios.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const users = require('./usuarios.json');
 
 exports.handler = async () => {
   try {
-    const usersFile = path.join(__dirname, "usuarios.json");
-    const users = JSON.parse(fs.readFileSync(usersFile, "utf-8"));
+    // const usersFile = path.join(__dirname, "usuarios.json");
+    // const users = JSON.parse(fs.readFileSync(usersFile, "utf-8"));
     return {
       statusCode: 200,
       body: JSON.stringify(users),
