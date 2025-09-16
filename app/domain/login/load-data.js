337da1beaaa735d5b9e3d4c969849f13e4ccf60e
xTUX3Rx <joseabantotrigoso@gmail.com>
2025-09-16 04:28:38 -0500

    Update load-data.js
    verificar usuario local

--- app/domain/login/load-data.js
+++ app/domain/login/load-data.js
@@ -3,6 +3,27 @@
     .then(response => response.json())
     .then(data => {
       localStorage.setItem('usuario', JSON.stringify(data));
+      //de aqui agregue abajo
+    // Verificar si hay credenciales guardadas
+      const savedUsername = localStorage.getItem('savedUsername');
+      const savedPassword = localStorage.getItem('savedPassword');
+
+      if (savedUsername && savedPassword) {
+        // Buscar usuario en el JSON cargado
+        const userFound = data.find(user =>
+          user.username === savedUsername && user.password === savedPassword
+        );
+
+        if (userFound) {
+          // Redirigir al usuario si las credenciales son válidas
+         window.location.href = '../../app/components/home/home.component.html';
+        } else {
+          // Credenciales inválidas, eliminarlas
+          localStorage.removeItem('savedUsername');
+          localStorage.removeItem('savedPassword');
+        }
+      }
+      //hasta aca se modifico
     })
     .catch(error => console.error('Error al cargar el JSON:', error));
 })();
